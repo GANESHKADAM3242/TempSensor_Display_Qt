9e3f4785a4245d5ac84a1664a7816cf18e6d0001
GANESHKADAM3242 <ganeshkadam3242@gmail.com>
2026-05-01 11:23:24 +0530

    Created class for Read data from Temerature sensor

--- sensorworker.cpp
+++ sensorworker.cpp
@@ -1,5 +1,29 @@
 #include "sensorworker.h"
+#include <QThread>
+#include <QDebug>
+#include <QRandomGenerator>
 
-SensorWorker::SensorWorker(QObject *parent)
+SensorWorker::SensorWorker(SensorData *data, QMutex *mutex, QObject *parent)
     : QObject{parent}
 {}
+
+void SensorWorker::startReading()
+{
+    m_running = true;
+
+    while(m_running)
+    {
+        QMutexLocker locker(m_mutex); // mutex lock here.
+        m_data->temperature = 20.0 + QRandomGenerator::global()->bounded(15);
+        m_data->humidity    = 40.0 + QRandomGenerator::global()->bounded(15);
+        m_data->readCount = m_data->readCount + 1;
+    }
+    QThread::msleep(500);
+    emit finished();
+
+}
+
+void SensorWorker::stop()
+{
+    m_running = false;
+}
--- sensorworker.h
+++ sensorworker.h
@@ -1,15 +1,34 @@
 #ifndef SENSORWORKER_H
 #define SENSORWORKER_H
 
+#pragma once
 #include <QObject>
+#include <QMutex>
+
+struct SensorData
+{
+    double temperature = 0.0;
+    double humidity    = 0.0;
+    int readCount      = 0;
+};
 
 class SensorWorker : public QObject
 {
     Q_OBJECT
 public:
-    explicit SensorWorker(QObject *parent = nullptr);
+    explicit SensorWorker(SensorData *data, QMutex *mutex, QObject *parent = nullptr);
+
+public slots:
+        void startReading();
+        void stop();
 
 signals:
+        void finished();
+
+private:
+        SensorData *m_data;
+        QMutex *m_mutex;
+        bool m_running = false;
 };
 
 #endif // SENSORWORKER_H
